80197ffbb32ae291e2804025044d5c3f4df91fa1
STemple <sean.temple.dev@gmail.com>
2024-08-20 13:13:17 -0600

    player on screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -9,14 +10,24 @@ def main():
         quit(1)
     if not pygame.display.get_init():
         quit(1)
+    clock = pygame.time.Clock()
+    delta = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         #game loop
-        screen.fill((0,0,0),)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+
+        # end of loop
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
